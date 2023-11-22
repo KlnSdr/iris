ae362aadb952be4f10d9625d3ee1d4d0ec30ba8b
KlnSdr <monsieurk1209@gmail.com>
2023-11-22 20:58:18 +0100

    cleanup code

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,6 @@ int main() {
     std::ios::sync_with_stdio(false);
     const int bufferSize = 256;
     char buffer[bufferSize];
-//    unsigned int index = 0;
-
-//    while (io::hasData()) {
-//        while (index < bufferSize) {
-//            char tmp = io::readByte();
-//            buffer[index] = tmp;
-//            index++;
-//
-//            if (std::cin.eof()) {
-//                break;
-//            }
-//        }
-//
-//        if(index > 0) {
-//            std::cout.write(buffer, index);
-//            index = 0;
-//        }
-//    }
 
     while (io::hasData()) {
         unsigned int sizeLeft = io::readBuffer(buffer, bufferSize);
