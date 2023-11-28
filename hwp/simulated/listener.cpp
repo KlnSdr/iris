bf12d2283fe1990f6073d7eafec42405cdfa98f2
KlnSdr <monsieurk1209@gmail.com>
2023-11-28 21:06:10 +0100

    make it FASTER

--- hwp/simulated/listener.cpp
+++ hwp/simulated/listener.cpp
@@ -22,37 +22,41 @@ void sleepMs(int ms) {
 int main() {
     SimClient::connectSim();
 
-	int buffer = 0;
-	int offset = 0;
-	bool wasNull = true;
-	int pause = 0;
+    int buffer = 0;
+    int offset = 0;
+    bool wasNull = true;
+    int pause = 0;
+    int bigPause = 0;
 
-	while(1) {
-		int value = readPins();
-		if (value != 0 && wasNull) {
-			pause = 0;
+    while(bigPause < 500) {
+        int value = readPins();
+        if (value != 0 && wasNull) {
+            bigPause = 0;
+            pause = 0;
 
-			buffer <<= offset;
-			buffer += value;
-			offset += 4;
-			wasNull = false;
+            buffer <<= offset;
+            buffer += value;
+            offset += 4;
+            wasNull = false;
 
-			if (offset > 4) {
-				std::cout << std::hex << buffer << " => " << static_cast<char>(buffer) << std::endl;
-				offset = 0;
-				buffer = 0;
-			}
-		} else if (value == 0 && !wasNull) {
-			wasNull = true;
-		} else if (value == 0 && wasNull) {
-			pause++;
-		}
+            if (offset > 4) {
+                // std::cout << std::hex << buffer << " => " << static_cast<char>(buffer) << std::endl;
+                std::cout << static_cast<char>(buffer) << std::flush;
+                offset = 0;
+                buffer = 0;
+            }
+        } else if (value == 0 && !wasNull) {
+            wasNull = true;
+        } else if (value == 0 && wasNull) {
+            pause++;
+            bigPause++;
+        }
 
-		if (pause >= 6) {
-			buffer = 0;
-			offset = 0;
-			pause = 0;
-		}
-		sleepMs(10);
-	}
+        if (pause >= 6) {
+            buffer = 0;
+            offset = 0;
+            pause = 0;
+        }
+        sleepMs(5);
+    }
 }
