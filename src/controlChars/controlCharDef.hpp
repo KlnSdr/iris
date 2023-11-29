16a4b2eb5b2b19165d80efe99a370f080464f79a
KlnSdr <monsieurk1209@gmail.com>
2023-11-29 22:56:11 +0100

    reformat control chars and add some docu to the esc chars

--- src/controlChars/controlCharDef.hpp
+++ src/controlChars/controlCharDef.hpp
@@ -1,5 +1,15 @@
-enum control {packageStart = 0x09, packageEnd = 0x0A,transmissionEnd = 0x0B,acknowledge = 0x0D,resend = 0x0E,esc1 = 0x07,esc2 = 0x05};
-
-
-
-
+enum control {
+    packageStart = 0x09,
+    packageEnd = 0x0A,
+    transmissionEnd = 0x0B,
+    acknowledge = 0x0D,
+    resend = 0x0E,
+    /**
+     * @brief escape character used to indicate that the next nibble is inverted
+     */
+    esc1 = 0x07,
+    /**
+     * @brief escape character used to indicate that the next nibble is data not a command
+     */
+    esc2 = 0x05
+};
