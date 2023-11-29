ee903674afdae1dc3d9693816d60fa23a19ea7af
Sebastian Schekahn <sebastian.schekahn@stud.htwk-leipzig.de>
2023-11-29 17:09:20 +0100

    Enums für die Steuerzeichen eingeführt.

---
+++ src/controlChars/controlCharDef.hpp
@@ -0,0 +1,5 @@
+enum control {packageStart = 0x09, packageEnd = 0x0A,transmissionEnd = 0x0B,acknowledge = 0x0D,resend = 0x0E,esc1 = 0x07,esc2 = 0x05};
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "io/io.hpp"
 #include "config/config.hpp"
+#include "controlChars/controlCharDef.hpp"
 
 int main() {
     config::setup();
@@ -9,5 +10,16 @@ int main() {
         unsigned int sizeLeft = io::readBuffer(buffer, config::bufferSize);
         std::cout.write(buffer, config::bufferSize - sizeLeft);
     }
+
+    enum control obj = resend;
+    enum control obj1 = packageStart;
+    enum control obj3 = resend;
+    std::cout << std::hex << obj1 + 1 << std::endl;
+    std::cout << std::hex << obj3 + 1 << std::endl;
+
+    std::cout << std::hex << obj + 1 << std::endl;
+
+
+
     return 0;
 }
