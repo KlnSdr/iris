5149a6f46387ab912cf1372efde3faf3e2c81ab3
Kilian <59069848+KlnSdr@users.noreply.github.com>
2023-11-30 19:14:11 +0100

    Add files via upload

--- hwp/sender.cpp
+++ hwp/sender.cpp
@@ -3,53 +3,66 @@
 #include <string> 
 #include <array>
 
-int main(){
-
-B15F& drv = B15F::getInstance();
-drv.setRegister(&DDRA, 0x0f);
-
-drv.setRegister(&PORTA, 0);
-drv.delay_ms(5000);
-int a = 0xFF1;
-char b = 'O';
-char let[5] = {'A', 'B', 'C','D','E'};
-
-
-int c = (int) (b);
-
-
-
-int x = b & 0xF0;
-int y = b & 0x0F;
-int z= x>>4;
-std::cout << z << std::endl;
-
-
-
-
-
-int i= 0;
-
-
-while(i<=4){
-
-int m = (int)let[i] & 0xF0;
-int n = (int)let[i] & 0x0F;
-int o= m>>4;
-i++;
-
-drv.setRegister(&PORTA,o);
-drv.delay_ms(500);
-drv.setRegister(&PORTA, 0);
-drv.delay_ms(500);
-drv.setRegister(&PORTA,n);
-drv.delay_ms(500);
-drv.setRegister(&PORTA,0);
-drv.delay_ms(1500);
+void sendNibble(char value, B15F& drv) {
+    drv.setRegister(&PORTA, value);
+    drv.delay_ms(500);
+    std::cout << "+" << std::hex << static_cast<int>(value) << std::endl;
 }
 
+int main(){
+    const char ESC = 0xA;
+    const char BEGIN = 0x5;
+    const char END = 0x7;
+    const char ESC2 = 0xD;
+
+    B15F& drv = B15F::getInstance();
+    drv.setRegister(&DDRA, 0x0f);
+
+    drv.setRegister(&PORTA, 0x0);
+    drv.delay_ms(5000);
+
+    char let[11] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
 
 
+    int i = 0;
+    char lastNibble = BEGIN;
+
+    sendNibble(BEGIN, drv);
+
+    while( i <= 10 ){
+        int leftNibble = (int)let[i] >> 4;
+        int rightNibble = (int)let[i] & 0x0F;
+        i++;
+
+        if (leftNibble == ESC || leftNibble == END) {
+            sendNibble(ESC2, drv);
+        } else if (leftNibble == ESC2 || leftNibble == lastNibble) {
+            leftNibble = (~leftNibble) & 0xF;
+            if (leftNibble == ESC) {
+                sendNibble(ESC2, drv);
+                leftNibble = (~leftNibble) & 0xF;
+            } else {
+                sendNibble(ESC, drv);
+            }
+        }
+        sendNibble(leftNibble, drv);
 
 
+        if (rightNibble == ESC || rightNibble == END) {
+            sendNibble(ESC2, drv);
+        } else if (rightNibble == ESC2 || rightNibble == leftNibble) {
+            rightNibble = (~rightNibble) & 0xF;
+            if (rightNibble == ESC) {
+                sendNibble(ESC2, drv);
+                rightNibble = (~rightNibble) & 0xF;
+            } else {
+                sendNibble(ESC, drv);
+            }
+        }
+        sendNibble(rightNibble, drv);
+
+        lastNibble = rightNibble;
+    }
+    sendNibble(END, drv);
+    sendNibble(0, drv);
 }
