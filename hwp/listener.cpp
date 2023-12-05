99362c668e39db16329ed7b2332e516cfaf731ed
KlnSdr <monsieurk1209@gmail.com>
2023-12-05 21:36:58 +0100

    parallel send/receive

--- hwp/listener.cpp
+++ hwp/listener.cpp
@@ -1,45 +1,107 @@
-#include <iostream>
-#include <b15f/b15f.h>
-#include <iomanip>
+#include "listener.hpp"
 
-using namespace std;
+const char Reader::esc = 0x0A;
+bool Reader::escbool = false;
+const char Reader::esc2 = 0x0D;
+bool Reader::esc2bool = false;
+int Reader::beginzaehler = 1;
+const char Reader::begin = 0x05;
+bool Reader::beginbool= false;
+const char Reader::end = 0x07;
+bool Reader::endbool = false;
+int Reader::buffer = 0;
+int Reader::offset = 0;
+bool Reader::wasNull = true;
+int Reader::pause = 0;
+int Reader::compareWert = 0;
 
-int main() {
-	B15F& drv = B15F::getInstance();
-	drv.setRegister(&DDRA, 0x00);
+char Reader::normalizeReading(char rawRead) {
+    char normalized = 0;
+    char shifted = rawRead >> 4;
 
-	int buffer = 0;
-	int offset = 0;
-	bool wasNull = true;
-	int pause = 0;
+    for (int i = 0; i < 4; i++) {
+        char lsb = shifted & 0x1;
+        lsb <<= 3 - i;
+        normalized |= lsb;
+        shifted >>= 1;
+    }
 
-	while(1) {
-		int value = (int) drv.getRegister(&PINA);
-		std::cout << std::hex << value << std::endl;
-		if (value != 0 && wasNull) {
-			pause = 0;
-
-			buffer <<= offset;
-			buffer += value;
-			offset += 4;
-			wasNull = false;
-
-			if (offset > 4) {
-				std::cout << std::hex << buffer << " => " << static_cast<char>(buffer) << std::endl;
-				offset = 0;
-				buffer = 0;
-			}
-		} else if (value == 0 && !wasNull) {
-			wasNull = true;
-		} else if (value == 0 && wasNull) {
-			pause++;
-		}
-
-		if (pause >= 6) {
-			buffer = 0;
-			offset = 0;
-			pause = 0;
-		}
-		drv.delay_ms(250);
-	}
+    return normalized;
 }
+
+void Reader::read(B15F& drv) {
+		
+    char value = normalizeReading((char) drv.getRegister(&PINA));
+    if (compareWert == value){
+        pause++;
+        return;
+    } else if (pause > 20) {
+        buffer = 0;
+        endbool = true;
+        beginbool = false;
+        std::cout << std::endl;
+	pause = 0;
+        return;
+    }
+    pause = 0;
+    compareWert = value;
+
+    // std::cout << "> " << std::hex << (int) value << std::endl;
+
+    if (escbool == true) {
+        value = (~value) & 0xF;
+        // std::cout << "inv " << std::hex << (int) value << std::endl;
+        escbool = false;
+    }
+
+    if(value == esc2){
+        esc2bool = true;
+        
+    }
+
+    if (value == esc) {
+        escbool = true;
+    }
+
+    if(value == begin && beginbool == false){
+        beginbool = true;
+        endbool = false;
+        return;
+    }
+    
+    
+    if(value == end && esc2bool == true){
+        endbool = false;
+        esc2bool = false;
+        
+    } else if(value == end && esc2bool == false){
+        endbool = true;
+        beginbool = false;
+        std::cout << std::endl;
+    }
+    
+    if (beginbool == true && endbool == false && esc2bool == false && escbool == false) {
+    
+        buffer <<= offset;
+        buffer += value;
+        offset += 4;
+        wasNull = false;
+
+        if (offset > 4){
+            std::cout << static_cast<char>(buffer) << std::flush;
+            // std::cout << "+ " << std::hex << (int) buffer << " -> " << static_cast<char>(buffer) << std::endl;
+            offset = 0;
+            buffer = 0;
+        }
+
+        if (pause >= 6) {
+            buffer = 0;
+            offset = 0;
+            pause = 0;
+        }
+    
+    }
+
+}
+
+
---
+++ hwp/listener.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <b15f/b15f.h>
+#include <iomanip>
+
+class Reader{
+public:
+    static void read(B15F& drv);
+private:
+    static const char esc;
+    static bool escbool;
+    static const char esc2;
+    static bool esc2bool;
+    static int beginzaehler;
+    static const char begin;
+    static bool  beginbool;
+    static const char end;
+    static bool endbool;
+    static int buffer;
+    static int offset;
+    static bool wasNull;
+    static int pause;
+    static int compareWert;
+    static char normalizeReading(char rawRead);
+};
